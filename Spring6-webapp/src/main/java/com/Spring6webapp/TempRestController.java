2fce8a9672f0f1c11c8b594ddaeac0f94cdd9f3c
Abhi <abhi17patel@gmail.com>
2023-12-07 13:08:48 -0500

    aDefine Custom Properties to application.properties and calling in RestController Endpoints

--- Spring6-webapp/src/main/java/com/Spring6webapp/TempRestController.java
+++ Spring6-webapp/src/main/java/com/Spring6webapp/TempRestController.java
@@ -1,6 +1,7 @@
 package com.Spring6webapp;
 
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,5 +18,16 @@ public class TempRestController {
         return "Run a Hard 5k!";
     }
 
+    //inject custom Application Properties
+    @Value("${coach.name}")
+    private String coachName;
+    @Value("${team.name}")
+    private String teamName;
+
+    //generate new endpoints
+    @GetMapping("/teaminfo")
+    public String getTeamInfo(){
+        return coachName + ", Team Name: "+ teamName;
+    }
 
 }
