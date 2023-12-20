64060011466dbcdfc3d836b58bc5712073563890
Abhi <abhi17patel@gmail.com>
2023-12-20 15:52:08 -0500

    Adding new Branch for Testing, adding testCases for Dependecy injection

--- Spring6-webapp/src/test/java/com/Spring6webapp/Spring6WebappApplicationTests.java
+++ Spring6-webapp/src/test/java/com/Spring6webapp/Spring6WebappApplicationTests.java
@@ -1,10 +1,19 @@
 package com.Spring6webapp;
 
+import com.Spring6webapp.depedencyInjection.DemoController;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class Spring6WebappApplicationTests {
+
+	@Autowired
+	DemoController demoController;
+    @Test
+	void testDemoController(){
+		System.out.println(demoController.getDailyWorkout());
+	}
 
 	@Test
 	void contextLoads() {
