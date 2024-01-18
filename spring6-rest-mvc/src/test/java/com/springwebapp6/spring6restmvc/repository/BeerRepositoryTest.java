b318d9ab484ffaa60e662f33b982063687400f14
Abhi <abhi17patel@gmail.com>
2024-01-18 13:21:11 -0500

    JPA validation on Entities

--- spring6-rest-mvc/src/test/java/com/springwebapp6/spring6restmvc/repository/BeerRepositoryTest.java
+++ spring6-rest-mvc/src/test/java/com/springwebapp6/spring6restmvc/repository/BeerRepositoryTest.java
@@ -1,13 +1,16 @@
 package com.springwebapp6.spring6restmvc.repository;
 
 import com.springwebapp6.spring6restmvc.entities.Beer;
+import com.springwebapp6.spring6restmvc.model.BeerStyle;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
+import java.math.BigDecimal;
+
 import static org.assertj.core.api.Assertions.*;
 
-@DataJpaTest
+@DataJpaTest //DataBase Testing
 class BeerRepositoryTest {
 
     @Autowired
@@ -16,8 +19,14 @@ class BeerRepositoryTest {
     @Test
     void testSaveBeer(){
         Beer savedBeer = beerRepository.save(Beer.builder()
-                .beerName("AB_Beer").build());
+                .beerName("AB_Beer")
+                .beerStyle(BeerStyle.ALE)
+                .upc("4585236")
+                .price(new BigDecimal("11.78"))
+                .build());
 
+        //need to add flush() method while testing Validation
+        beerRepository.flush();
         assertThat(savedBeer).isNotNull();
         assertThat(savedBeer.getId()).isNotNull();
     }
