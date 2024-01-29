a65b142c96fd08cd7b0d0ca3443f5cd97cbf762c
Abhi <abhi17patel@gmail.com>
2024-01-29 15:53:28 -0500

    Create BeerOrderRepository, Create BeerOrderRepositoryTest

---
+++ spring6-rest-mvc/src/main/java/com/springwebapp6/spring6restmvc/repository/BeerOrderRepository.java
@@ -0,0 +1,9 @@
+package com.springwebapp6.spring6restmvc.repository;
+
+import com.springwebapp6.spring6restmvc.entities.BeerOrder;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface BeerOrderRepository extends JpaRepository<BeerOrder, UUID> {
+}
