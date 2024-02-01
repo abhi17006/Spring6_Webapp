c9f969b65c075fa46681152ea5c32a27e7245e99
Abhi <abhi17patel@gmail.com>
2024-02-01 13:20:58 -0500

    adding Sping-rest-template project

---
+++ spring6-rest-template/src/test/java/com/springresttemplate/Spring6RestTemplateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springresttemplate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring6RestTemplateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
