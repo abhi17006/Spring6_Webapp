cd0fa5bf0e1946be68ef3777edb73113dfe2f87e
Abhi <abhi17patel@gmail.com>
2024-02-01 13:13:56 -0500

    Add Spring-Data-Rest resources

--- Spring-data-rest/src/main/java/com/springdatarest/springdatarest/repositories/BeerRepository.java
+++ Spring-data-rest/src/main/java/com/springdatarest/springdatarest/repositories/BeerRepository.java
@@ -5,9 +5,12 @@ import com.springdatarest.springdatarest.domain.BeerStyleEnum;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import java.util.UUID;
 
+//using Data-REST resources, change base path
+@RepositoryRestResource(path = "beer")
 public interface BeerRepository extends JpaRepository<Beer, UUID> {
     Page<Beer> findAllByBeerName(String beerName, Pageable pageable);
 
