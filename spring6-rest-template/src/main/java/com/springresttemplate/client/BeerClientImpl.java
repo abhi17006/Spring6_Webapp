a517bed5c449b2cd91989b885ae51da9c2f45621
Abhi <abhi17patel@gmail.com>
2024-02-01 17:03:44 -0500

    Spring Pageable with jackson to use Json anno

--- spring6-rest-template/src/main/java/com/springresttemplate/client/BeerClientImpl.java
+++ spring6-rest-template/src/main/java/com/springresttemplate/client/BeerClientImpl.java
@@ -1,12 +1,17 @@
 package com.springresttemplate.client;
 
+import com.fasterxml.jackson.databind.JsonNode;
 import com.springresttemplate.model.BeerDTO;
+import com.springresttemplate.model.BeerDTOPageImpl;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.Map;
 
 @Service
 @RequiredArgsConstructor
@@ -14,15 +19,37 @@ public class BeerClientImpl implements BeerClient {
 
     //call RestTemplateBuilder class t
     private final RestTemplateBuilder restTemplateBuilder;
-
+    private static final String BASE_URL = "http://localhost:8080";
+    private static final String GET_BEER_PATH = "/api/v1/beer";
     @Override
     public Page<BeerDTO> listBeers() {
         //
         RestTemplate restTemplate = restTemplateBuilder.build();
 
-        //using getFOrEntity method type of RepsonseEntity
-        ResponseEntity<String> responseEntity = restTemplate.getForEntity("http://localhost:8080/api/v1/beer",String.class);
-        System.out.println(responseEntity.getBody()); //print getBody data
+//        //using getFOrEntity method type of RepsonseEntity
+//        ResponseEntity<String> responseEntity = restTemplate.getForEntity(BASE_URL+ GET_BEER_PATH,String.class);
+//
+//        //map response, useful when you don't know the API response
+//        ResponseEntity<Map> mapResponseEntity =
+//                restTemplate.getForEntity(BASE_URL+GET_BEER_PATH, Map.class);
+//
+//        //work with Jackson library with JsonNode
+//        ResponseEntity<JsonNode> jsonNodeResponseEntity =
+//                restTemplate.getForEntity(BASE_URL+GET_BEER_PATH, JsonNode.class);
+//
+//        //accessing JSOn response content attributes from the path
+//        jsonNodeResponseEntity.getBody().findPath("content")
+//                        .elements().forEachRemaining(node -> {
+//                    System.out.println(node.get("beerName").asText());
+//                });
+//
+//        System.out.println(mapResponseEntity.getBody()); //print getBody data
+
+
+
+        //using getFOrEntity method type of RepsonseEntity with jackson libray and Page
+        ResponseEntity<BeerDTOPageImpl> responseEntity =
+                restTemplate.getForEntity(BASE_URL+ GET_BEER_PATH,BeerDTOPageImpl.class);
         return null;
     }
 }
